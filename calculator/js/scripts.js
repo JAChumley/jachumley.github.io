723d949ef6e51da8afcf428e633e8816d27896df
JAChumley <jestaar@gmail.com>
2016-10-16 14:05:39 -0700

    Calculator project

---
+++ calculator/js/scripts.js
@@ -0,0 +1,33 @@
+
+
+ box=document.getElementById('display');
+
+  function addtoscreen(x) {
+    box.value+=x;
+
+      if (x=='c') {
+        box.value='';
+      }
+  }
+
+  function answer() {
+    x=box.value;
+    x=eval(x);
+    box.value=x;
+  }
+
+  function backspace() {
+    number=box.value;
+    len=number.length-1;
+    newnumber=number.substring(0,len);
+    box.value=newnumber;
+
+  }
+
+  function power(y) {
+    x=box.value;
+    x=Math.pow(x,y);
+    box.value=x;
+
+
+  }
